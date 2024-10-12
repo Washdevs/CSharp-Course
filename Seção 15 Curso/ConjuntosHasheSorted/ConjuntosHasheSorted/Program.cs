d9ea688ec3849e060ec4d05b405d572ae4675941
Washington <lepex102000@gmail.com>
2024-10-12 12:21:52 -0300

    Sorted

---
+++ Seção 15 Curso/ConjuntosHasheSorted/ConjuntosHasheSorted/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConjuntosHasheSorted
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach (string s in set)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ Seção 15 Curso/Sorted/Sorted/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorted
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<int> a = new SortedSet<int>() { 0, 3, 2, 4, 5, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            PrintCollection(c);
+        }
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach (T item in collection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
